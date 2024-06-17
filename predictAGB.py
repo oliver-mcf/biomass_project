bfb67514bf6301e72ae099a78d54da61d8d3ca97
s1949330 <s1949330@ed.ac.uk>
2024-06-18 00:48:04 +0100

    working script to compare model variable performances in advance of presentation

--- predictAGB.py
+++ predictAGB.py
@@ -13,7 +13,7 @@ def resample(reference_path, geotiff_list):
         ref_transform = ref_dataset.transform
         ref_crs = ref_dataset.crs
         ref_res = (ref_transform.a, -ref_transform.e)
-    # Iterate over the list with tqdm
+    # Iterate over variable list
     for idx in tqdm(range(len(geotiff_list)), desc = 'Resampling predictor variables...'):
         geotiff_path = geotiff_list[idx]
         with rio.open(geotiff_path) as src_dataset:
@@ -37,8 +37,7 @@ def resample(reference_path, geotiff_list):
                               resampling = Resampling.bilinear)
     print('Resampling complete')
 
-def prepare(site, year):
-    # Read predictor variables
+def prepare(site, year, batch_proportion=0.1):
     vars_files = glob(f'/home/s1949330/Documents/scratch/diss_data/pred_vars/{site}/{year}_*.tif')
     nVars = len(vars_files)
     pred_vars = []
@@ -47,27 +46,38 @@ def prepare(site, year):
             pred_vars.append(dataset.read(1))
     nY, nX = pred_vars[0].shape
     pred_vars_flat = np.empty((nX * nY, nVars), dtype=float)
-    chunk_size = 1000
-    for i in range(0, nX * nY, chunk_size):
-        chunk_end = min(i + chunk_size, nX * nY)
-        for j in range(nVars):
-            band_data = pred_vars[j].flatten()
-            pred_vars_flat[i:chunk_end, j] = band_data[i:chunk_end]
+    batch_size = int(nX * nY * batch_proportion)
+    print('Flattening predictor variables...')
+    with tqdm(total=nX * nY) as progress_bar:
+        for i in range(0, nX * nY, batch_size):
+            chunk_end = min(i + batch_size, nX * nY)
+            for j in range(nVars):
+                band_data = pred_vars[j].flatten()
+                if chunk_end > len(band_data):
+                    chunk_end = len(band_data)
+                pred_vars_flat[i:chunk_end, j] = band_data[i:chunk_end]
+            progress_bar.update(chunk_end - i)
     print('Predictor variables flattened')
     return pred_vars_flat
 
-def predict(pred_vars_flat, model_path, batch_size = 10000):
+def predict(pred_vars_flat, model_path, no_data_value=-999, batch_proportion=0.1):
     rf = joblib.load(model_path)
     n_samples, n_features = pred_vars_flat.shape
+    print(n_features)
+    batch_size = int(n_samples * batch_proportion)
     predictions = np.empty((n_samples,), dtype=float)
-    progress_bar = tqdm(range(0, n_samples, batch_size), desc = 'Predicting AGB...')
-    for i in progress_bar:
-        batch_end = min(i + batch_size, n_samples)
-        batch_data = pred_vars_flat[i:batch_end, :]
-        batch_pred = rf.predict(batch_data)
-        predictions[i:batch_end] = batch_pred
-    progress_bar.close()
-    print('Successful predicted AGB')
+    print('Predicting AGB...')
+    with tqdm(total=n_samples) as progress_bar:
+        for i in range(0, n_samples, batch_size):
+            batch_end = min(i + batch_size, n_samples)
+            batch_data = pred_vars_flat[i:batch_end, :]
+            mask = np.any(batch_data == no_data_value, axis=1)
+            batch_data = batch_data[~mask]
+            batch_data = np.nan_to_num(batch_data, nan=0.0, posinf=1e9, neginf=-1e9)
+            batch_pred = rf.predict(batch_data)
+            predictions[i:batch_end][~mask] = batch_pred
+            progress_bar.update(batch_end - i)
+    print('Successfully predicted AGB')
     print(f'Max: {np.max(predictions)} Mg/ha')
     print(f'Mean: {np.mean(predictions)} Mg/ha')
     print(f'Median: {np.median(predictions)} Mg/ha')
@@ -95,19 +105,35 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     # Resample data to match resolutions
-    reference_path = f'/home/s1949330/Documents/scratch/diss_data/pred_vars/REFERENCE_{args.site}_23_SR_B2_Median.tif'
-    geotiff_list = glob(f'/home/s1949330/Documents/scratch/diss_data/pred_vars/{args.site}/{args.year}*.tif')
+    #reference_path = f'/home/s1949330/Documents/scratch/diss_data/pred_vars/REFERENCE_{args.site}_23_SR_B2_Median.tif'
+    #geotiff_list = glob(f'/home/s1949330/Documents/scratch/diss_data/pred_vars/{args.site}/{args.year}*.tif')
     #resample(reference_path, geotiff_list)
 
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     # Read and flatten predictor variables
-    pred_vars_flat = prepare(args.site, args.year)
+    #pred_vars_flat = prepare(args.site, args.year)
 
     # Predict AGB across study area
-    model = f'/home/s1949330/Documents/scratch/diss_data/model/{args.model}/RF_MODEL.joblib'
-    predictions = predict(pred_vars_flat, model)
+    #model = f'/home/s1949330/Documents/scratch/diss_data/model/{args.model}/RF_MODEL.joblib'
+    #predictions = predict(pred_vars_flat, model)
 
     # Map predicted AGB for study area
-    with rio.open(reference_path) as reference:
-        map_biomass(args.site, args.year, predictions, reference)
+    #with rio.open(reference_path) as reference:
+    #    map_biomass(args.site, args.year, predictions, reference)
 
     # Save map as geotiff
