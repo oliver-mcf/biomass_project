1083fc1bcbc098c630ede3154755795e29f4b872
s1949330 <s1949330@ed.ac.uk>
2024-06-27 14:03:20 +0100

    model plot function changed to scatter and added command line argument for RF trees - awaiting script for testing trees

--- trainModel.py
+++ trainModel.py
@@ -22,16 +22,16 @@ def isolate(filename, label):
         x = df.loc[:, 'SR_B2_Median' : 'HHHV_Index']
     else:
         raise ValueError("Invalid keyword specified.")
-    print('Available Points for Model Training: {:,}'.format(len(x)))
+    print('Training Data Sample Size: {:,}'.format(len(x)))
     return y, x, coords
 
 def split(x, y, split_ratio):
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = split_ratio)
     return x_train, x_test, y_train, y_test
 
-def train(x_train, y_train, label, site = ''):
+def train(x_train, y_train, label, trees, site = ''):
     print('Running Random Forest Algorithm ...')
-    rf = RandomForestRegressor(n_estimators = 200, random_state = random.seed())
+    rf = RandomForestRegressor(n_estimators = trees, random_state = random.seed())
     rf.fit(x_train, y_train)
     model_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{site}_{label}_RF_MODEL.joblib' if site else f'/home/s1949330/Documents/scratch/diss_data/model/{label}_RF_MODEL.joblib'
     joblib.dump(rf, model_filename)
@@ -47,10 +47,10 @@ def test(x_test, y_test, label, site = ''):
     print(f'MAE%: {(np.mean(np.abs(y_test - y_pred)) / np.mean(y_test)) * 100:.3f} %')
     print(f'RMSE: {sqrt(mean_squared_error(y_test, y_pred)):.3f} Mg/ha')
     print(f'RMSE%: {(sqrt(mean_squared_error(y_test, y_pred)) / np.mean(y_test)) * 100:.3f} %')
-    residuals = y_pred - y_test
-    constant = sm.add_constant(y_test)
-    white_test = sm.stats.diagnostic.het_white(residuals, constant)
-    print(f'LM/F/P: {white_test[0]:.0f} / {white_test[2]:.0f} / {white_test[1]:.2f}')
+    #residuals = y_pred - y_test
+    #constant = sm.add_constant(y_test)
+    #white_test = sm.stats.diagnostic.het_white(residuals, constant)
+    #print(f'LM/F/P: {white_test[0]:.0f} / {white_test[2]:.0f} / {white_test[1]:.2f}')
     return y_pred
 
 def variable_importance(label, var_names, site = ''):
@@ -66,26 +66,24 @@ def variable_importance(label, var_names, site = ''):
             scaled_importance = importances[idx] * 100
             writer.writerow([var_names[idx], scaled_importance])
 
-'''''''''
-def model_plot(y_test, y_pred):
+def model_plot(y_test, y_pred, label):
     plt.rcParams['font.family'] = 'Arial'
-    fig, ax = plt.subplots(figsize = (10, 8))
-    hist = ax.hist2d(y_test, y_pred, bins = 30, cmap = 'cividis', cmin = 1)
+    fig, ax = plt.subplots(figsize=(10, 8))
+    ax.scatter(y_test, y_pred, color = 'blue', alpha = 0.5, edgecolor = 'k')
     ax.plot([0, 200], [0, 200], ls = 'solid', color = 'k')
-    sat = y_pred < 200
-    slope, intercept = np.polyfit(y_test[sat], y_pred[sat], 1)
-    best_fit = slope * np.array(y_test[sat]) + intercept
-    ax.plot(y_test[sat], best_fit, ls = 'solid', color = 'red')
+    slope, intercept = np.polyfit(y_test, y_pred, 1)
+    best_fit = slope * np.array(y_test) + intercept
+    ax.plot(y_test, best_fit, ls = 'solid', color = 'red', label = 'Line of Best Fit')
     ax.set_xlim([0, 200])
     ax.set_ylim([0, 200])
-    ax.set_xticks(np.arange(0, 220, 20))
-    ax.set_yticks(np.arange(0, 220, 20))
-    cbar = plt.colorbar(hist[3], ax = ax, label = 'Count', shrink = 0.6)
+    ax.set_xticks(np.arange(0, 210, 10))
+    ax.set_yticks(np.arange(0, 210, 10))
     ax.set_xlabel('Observed AGB (Mg/ha)')
     ax.set_ylabel('Predicted AGB (Mg/ha)')
-    plt.savefig('MODEL_TEST.png')
+    ax.legend()
+    plt.savefig(f'/home/s1949330/Documents/scratch/diss_data/model/{label}_MODEL_TEST.png')
     plt.show()
-'''''''''
+
 
 
 # Code #############################################################################################################
@@ -93,8 +91,9 @@ if __name__ == '__main__':
     
     # Define command line arguments
     parser = argparse.ArgumentParser(description = 'Random Forest Model Training and Evaluation')
-    parser.add_argument('label', type = str, choices = ['Landsat', 'Sentinel', 'Palsar', 'All'], help = 'Keyword for selecting predictor variables')
-    parser.add_argument('--split', type = float, default = 0.3, help = 'Ratio for splitting the data into training and testing sets (Default: 0.3)')
+    parser.add_argument('--label', type = str, choices = ['Landsat', 'Sentinel', 'Palsar', 'All'], help = 'Keyword for selecting predictor variables')
+    parser.add_argument('--split', type = float, default = 0.3, help = 'Ratio for splitting the data into training and testing sets; default = 0.3')
+    parser.add_argument('--trees', type = str, help = 'Number of estimators to train random forest model')
     parser.add_argument('--site', type = str, default = '', help = 'Site name to filter CSV files (optional)')
     args = parser.parse_args()
     
@@ -118,10 +117,11 @@ if __name__ == '__main__':
     testing_data.to_csv(testing_data_filename, index = False)
 
     # Train RF model
-    train(x_train, y_train, args.label, args.site)
+    train(x_train, y_train, args.label, args.trees, args.site)
 
     # Test RF model performance
     y_pred = test(x_test, y_test, args.label, args.site)
+    model_plot(y_test, y_pred, args.label)
 
     # Variable importance
     variable_importance(args.label, x.columns, args.site)
