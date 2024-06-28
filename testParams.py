afdd64ee061c931d7dc1bfded1a46b331c00aa00
s1949330 <s1949330@ed.ac.uk>
2024-06-28 11:39:48 +0100

    script to iteratively run RF models to test best number of trees for larger model

---
+++ testParams.py
@@ -0,0 +1,49 @@
+
+# Test Random Forest Model Parameters
+
+# Libraries ########################################################################################################
+from libraries import *
+from trainModel import isolate, split
+
+
+# Objects & Methods ################################################################################################
+def param_train(x_train, y_train, trees):
+    rf = RandomForestRegressor(n_estimators = trees, random_state = random.seed())
+    rf.fit(x_train, y_train)
+    return rf
+
+def param_test(rf, x_test, y_test):
+    y_pred = rf.predict(x_test)
+    r2 = r2_score(y_test, y_pred)
+    return r2, y_pred
+
+
+# Code #############################################################################################################
+if __name__ == '__main__':
+    
+    # Define command line arguments
+    parser = argparse.ArgumentParser(description = 'Random Forest Model Training and Evaluation')
+    parser.add_argument('--label', type = str, choices = ['Landsat', 'Sentinel', 'Palsar', 'All', 'Test'], required = True, help = 'Keyword for selecting predictor variables')
+    parser.add_argument('--split', type = float, default = 0.3, help = 'Ratio for splitting the data into training and testing sets; default = 0.3')
+    parser.add_argument('--site', type = str, default = '', help = 'Site name to filter CSV files (optional)')
+    args = parser.parse_args()
+
+    # Isolate target and predictor variables
+    if args.site == '':
+        input_filename = '/home/s1949330/Documents/scratch/diss_data/model/MODEL_INPUT_FILTER.csv'
+    else:
+        input_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{args.site}_MODEL_INPUT_FILTER.csv'
+    y, x, coords = isolate(input_filename, args.label)
+
+    # Split sampled data for model training
+    x_train, x_test, y_train, y_test = split(x, y, split_ratio = args.split)
+
+    # List of n_estimators to iterate over
+    n_estimators_list = [100, 200, 300]
+
+    # Iterate over n_estimators, train and test the model
+    for n_estimators in n_estimators_list:
+        rf = param_train(x_train, y_train, n_estimators)
+        r2, y_pred = param_test(rf, x_test, y_test)
+        print(f'Trees: {n_estimators}, R2: {r2:.3f}')
+
