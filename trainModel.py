b110473a27180b66bd902d89e531c1e86547de79
s1949330 <s1949330@ed.ac.uk>
2024-06-28 11:38:37 +0100

    amended isolate function to better identify appropriate predictor variables, startswith

--- trainModel.py
+++ trainModel.py
@@ -13,15 +13,21 @@ def isolate(filename, label):
     y = df['GEDI_AGB']
     coords = df[['GEDI_X', 'GEDI_Y']]
     if label == "Landsat":
-        x = df.loc[:, 'SR_B2_Median' : 'SRTM_mTPI']
+        landsat_cols = [col for col in df.columns if col.startswith(('SRTM', 'SR', 'T'))]
+        x = df.loc[:, landsat_cols]
     elif label == "Sentinel":
-        x = df.loc[:, 'SRTM_mTPI' : 'VH_05']
+        sentinel_cols = [col for col in df.columns if col.startswith(('SRTM', 'VV', 'VH'))]
+        x = df.loc[:, sentinel_cols]
     elif label == "Palsar":
-        x = df.loc[:, 'SR_B2_Median' : 'HHHV_Index']
+        palsar_cols = [col for col in df.columns if col.startswith(('SRTM', 'HH', 'HV'))]
+        x = df.loc[:, palsar_cols]
     elif label == "All":
-        x = df.loc[:, 'SR_B2_Median' : 'HHHV_Index']
-    else:
-        raise ValueError("Invalid keyword specified.")
+        exclude_substrings = ['GEDI_X', 'GEDI_Y', 'GEDI_AGB']
+        combined_cols = [col for col in df.columns if not any(substring in col for substring in exclude_substrings)]
+        x = df.loc[:, combined_cols]
+    elif label == "Test":
+        test_cols = [col for col in df.columns if col.startswith(('SRTM', 'T'))]
+        x = df.loc[:, test_cols]
     print('Training Data Sample Size: {:,}'.format(len(x)))
     return y, x, coords
 
@@ -29,15 +35,15 @@ def split(x, y, split_ratio):
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = split_ratio)
     return x_train, x_test, y_train, y_test
 
-def train(x_train, y_train, label, trees, site = ''):
+def train(x_train, y_train, label):
     print('Running Random Forest Algorithm ...')
-    rf = RandomForestRegressor(n_estimators = trees, random_state = random.seed())
+    rf = RandomForestRegressor(n_estimators = 100, random_state = random.seed())
     rf.fit(x_train, y_train)
-    model_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{site}_{label}_RF_MODEL.joblib' if site else f'/home/s1949330/Documents/scratch/diss_data/model/{label}_RF_MODEL.joblib'
+    model_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{label}_RF_MODEL.joblib'
     joblib.dump(rf, model_filename)
 
-def test(x_test, y_test, label, site = ''):
-    model_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{site}_{label}_RF_MODEL.joblib' if site else f'/home/s1949330/Documents/scratch/diss_data/model/{label}_RF_MODEL.joblib'
+def test(x_test, y_test, label):
+    model_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{label}_RF_MODEL.joblib'
     rf = joblib.load(model_filename)
     y_pred = rf.predict(x_test)
     r2 = r2_score(y_test, y_pred)
@@ -53,12 +59,12 @@ def test(x_test, y_test, label, site = ''):
     #print(f'LM/F/P: {white_test[0]:.0f} / {white_test[2]:.0f} / {white_test[1]:.2f}')
     return y_pred
 
-def variable_importance(label, var_names, site = ''):
-    model_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{site}_{label}_RF_MODEL.joblib' if site else f'/home/s1949330/Documents/scratch/diss_data/model/{label}_RF_MODEL.joblib'
+def variable_importance(label, var_names):
+    model_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{label}_RF_MODEL.joblib'
     rf = joblib.load(model_filename)
     importances = rf.feature_importances_
     sorted_idx = np.argsort(importances)[::-1]
-    output_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{site}_{label}_VARIABLE_IMPORTANCE.csv' if site else f'/home/s1949330/Documents/scratch/diss_data/model/{label}_VARIABLE_IMPORTANCE.csv'
+    output_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{label}_VARIABLE_IMPORTANCE.csv'
     with open(output_filename, mode = 'w', newline = '') as file:
         writer = csv.writer(file)
         writer.writerow(['Variable', 'Importance (%)'])
@@ -91,14 +97,13 @@ if __name__ == '__main__':
     
     # Define command line arguments
     parser = argparse.ArgumentParser(description = 'Random Forest Model Training and Evaluation')
-    parser.add_argument('--label', type = str, choices = ['Landsat', 'Sentinel', 'Palsar', 'All'], help = 'Keyword for selecting predictor variables')
+    parser.add_argument('label', type = str, choices = ['Landsat', 'Sentinel', 'Palsar', 'All'], help = 'Keyword for selecting predictor variables')
     parser.add_argument('--split', type = float, default = 0.3, help = 'Ratio for splitting the data into training and testing sets; default = 0.3')
-    parser.add_argument('--trees', type = str, help = 'Number of estimators to train random forest model')
-    parser.add_argument('--site', type = str, default = '', help = 'Site name to filter CSV files (optional)')
+    parser.add_argument('--trees', type = str, default = 200, help = 'Number of estimators to train random forest model; default = 200')
     args = parser.parse_args()
     
     # Isolate target and predictor variables
-    input_filename = f'/home/s1949330/Documents/scratch/diss_data/model/csv/{args.site}_MODEL_INPUT.csv' if args.site else '/home/s1949330/Documents/scratch/diss_data/model/csv/MODEL_INPUT.csv'
+    input_filename = '/home/s1949330/Documents/scratch/diss_data/model/MODEL_INPUT_FILTER.csv'
     y, x, coords = isolate(input_filename, args.label)
 
     # Split data for model training
@@ -107,23 +112,19 @@ if __name__ == '__main__':
     # Save model training data
     training_data = pd.concat([coords, pd.DataFrame(x_train), pd.DataFrame({'y_train': y_train})], axis = 1).dropna(subset = ['y_train'])
     testing_data = pd.concat([coords, pd.DataFrame(x_test), pd.DataFrame({'y_test': y_test})], axis = 1).dropna(subset = ['y_test'])
-    if args.site:
-        training_data_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{args.site}_{args.label}_MODEL_TRAINING.csv'
-        testing_data_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{args.site}_{args.label}_MODEL_TESTING.csv'
-    else:
-        training_data_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{args.label}_MODEL_TRAINING.csv'
-        testing_data_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{args.label}_MODEL_TESTING.csv'
+    training_data_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{args.label}_MODEL_TRAINING.csv'
+    testing_data_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{args.label}_MODEL_TESTING.csv'
     training_data.to_csv(training_data_filename, index = False)
     testing_data.to_csv(testing_data_filename, index = False)
 
     # Train RF model
-    train(x_train, y_train, args.label, args.trees, args.site)
+    train(x_train, y_train, args.label)
 
     # Test RF model performance
-    y_pred = test(x_test, y_test, args.label, args.site)
+    y_pred = test(x_test, y_test, args.label)
     model_plot(y_test, y_pred, args.label)
 
     # Variable importance
-    variable_importance(args.label, x.columns, args.site)
+    variable_importance(args.label, x.columns)
 
 
