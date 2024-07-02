b6a85623e251a0ad054a8440bebad2b2be0927ea
s1949330 <s1949330@ed.ac.uk>
2024-07-02 13:57:02 +0100

    added cross validation package from sklearn

--- libraries.py
+++ libraries.py
@@ -11,10 +11,8 @@ import argparse
 from pprint import pprint
 import os
 import pandas as pd
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import train_test_split, KFold
 from sklearn.ensemble import RandomForestRegressor
-from sklearn import metrics
-from sklearn.metrics import r2_score
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 import joblib
 import random
