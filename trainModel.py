9323d27ca01762f9631388f83f01fa131f4ad86d
s1949330 <s1949330@ed.ac.uk>
2024-07-03 13:29:54 +0100

    working k-fold model training with 2d histogram and sample option available and working

--- trainModel.py
+++ trainModel.py
@@ -53,7 +53,6 @@ def save_splits(x_train, y_train, x_test, y_test, coords, args, fold = None):
     # Isolate subsets
     training_data = pd.concat([coords, pd.DataFrame(x_train), pd.DataFrame({'y_train': y_train})], axis = 1).dropna(subset = ['y_train'])
     testing_data = pd.concat([coords, pd.DataFrame(x_test), pd.DataFrame({'y_test': y_test})], axis = 1).dropna(subset = ['y_test'])
-    
     # Consider site-specific condition
     if args.site == '':
         training_data_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{args.folder}/{args.label}_MODEL_TRAINING.csv'
@@ -61,12 +60,10 @@ def save_splits(x_train, y_train, x_test, y_test, coords, args, fold = None):
     else:
         training_data_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{args.folder}/{args.site}_{args.label}_MODEL_TRAINING.csv'
         testing_data_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{args.folder}/{args.site}_{args.label}_MODEL_TESTING.csv'
-    
     # If fold is provided, append fold number to the filenames
     if fold is not None:
-        training_data_filename = training_data_filename.replace('.csv', f'_FOLD{fold}.csv')
-        testing_data_filename = testing_data_filename.replace('.csv', f'_FOLD{fold}.csv')
-    
+        training_data_filename = training_data_filename.replace('.csv', f'_FOLD{fold + 1}.csv')
+        testing_data_filename = testing_data_filename.replace('.csv', f'_FOLD{fold + 1}.csv')
     # Write splits to csv
     training_data.to_csv(training_data_filename, index = False)
     testing_data.to_csv(testing_data_filename, index = False)
@@ -80,7 +77,7 @@ def train_model(x_train, y_train, label, trees, folder, fold = None):
     # Save trained model
     model_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{folder}/{label}_RF_MODEL.joblib'
     if fold is not None:
-        model_filename = model_filename.replace('.joblib', f'_FOLD{fold}.joblib')
+        model_filename = model_filename.replace('.joblib', f'_FOLD{fold + 1}.joblib')
     joblib.dump(rf, model_filename)
 
 def test_model(x_test, y_test, folder, label, fold = None):
@@ -88,7 +85,8 @@ def test_model(x_test, y_test, folder, label, fold = None):
     # Read model
     model_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{folder}/{label}_RF_MODEL.joblib'
     if fold is not None:
-        model_filename = model_filename.replace('.joblib', f'_FOLD{fold}.joblib')
+        model_filename = model_filename.replace('.joblib', f'_FOLD{fold + 1}.joblib')
+    print(f"Loading: {os.path.basename(model_filename)}")
     rf = joblib.load(model_filename)
     # Statistically analyse model output
     y_pred = rf.predict(x_test)
@@ -103,8 +101,7 @@ def test_model(x_test, y_test, folder, label, fold = None):
         'RMSE': sqrt(mean_squared_error(y_test, y_pred)),
         'LM': white_test[0],
         'F': white_test[2],
-        'P': white_test[1]
-    }
+        'P': white_test[1]}
     print(f'R: {stats_dict["R"]}')
     print(f'R2: {stats_dict["R2"]:.3f}')
     print(f'Bias: {stats_dict["Bias"]:.3f} Mg/ha')
@@ -118,14 +115,14 @@ def variable_importance(folder, label, var_names, fold = None):
     # Read model
     model_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{folder}/{label}_RF_MODEL.joblib'
     if fold is not None:
-        model_filename = model_filename.replace('.joblib', f'_FOLD{fold}.joblib')
+        model_filename = model_filename.replace('.joblib', f'_FOLD{fold + 1}.joblib')
     rf = joblib.load(model_filename)
     importances = rf.feature_importances_
     sorted_idx = np.argsort(importances)[::-1]
     # Write variable importances to csv
     output_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{folder}/{label}_VARIABLE_IMPORTANCE.csv'
     if fold is not None:
-        output_filename = output_filename.replace('.csv', f'_FOLD{fold}.csv')
+        output_filename = output_filename.replace('.csv', f'_FOLD{fold + 1}.csv')
     with open(output_filename, mode = 'w', newline = '') as file:
         writer = csv.writer(file)
         writer.writerow(['Variable', 'Importance (%)'])
@@ -133,8 +130,8 @@ def variable_importance(folder, label, var_names, fold = None):
             scaled_importance = importances[idx] * 100
             writer.writerow([var_names[idx], scaled_importance])
 
-def model_scatter(y_test, y_pred, folder, label, fold = None):
-    '''Plot Observed and Predicted Values'''
+def model_scatter(y_test, y_pred, folder, label, model):
+    '''Plot Scatter of Observed and Predicted Values'''
     # Constrain values to model sensitivity
     mask = (y_test < 300) & (y_pred < 300)
     y_test = y_test[mask]
@@ -155,45 +152,63 @@ def model_scatter(y_test, y_pred, folder, label, fold = None):
     ax.set_xlabel('Observed AGB (Mg/ha)')
     ax.set_ylabel('Predicted AGB (Mg/ha)')
     ax.set_title(f'{label} Model - Observed vs Predicted')
-    if fold is not None:
-        plt.savefig(f'/home/s1949330/Documents/scratch/diss_data/model/{folder}/{label}_MODEL_SCATTER_FOLD{fold}.png')
-    else:
-        plt.savefig(f'/home/s1949330/Documents/scratch/diss_data/model/{folder}/{label}_MODEL_SCATTER.png')
+    plt.savefig(f'/home/s1949330/Documents/scratch/diss_data/model/{folder}/{label}_MODEL_SCATTER_FOLD{model + 1}.png', dpi = 300)
     plt.close(fig)
 
-def model_hist(y_test, y_pred, folder, label, fold = None):
-    '''Plot Histogram of Prediction Residuals'''
-    residuals = y_test - y_pred
+def model_hist(y_test, y_pred, folder, label, model):
+    '''Plot Histogram of Observed and Predicted Values'''
+    # Constrain values to model sensitivity
+    mask = (y_test < 300) & (y_pred < 300)
+    y_test = y_test[mask]
+    y_pred = y_pred[mask]
     plt.rcParams['font.family'] = 'Arial'
     fig, ax = plt.subplots(figsize = (14, 12))
-    ax.hist(residuals, bins = 50, color = 'lightsteelblue', edgecolor = 'steelblue')
-    ax.set_xlabel('Prediction Residuals (Mg/ha)')
-    ax.set_ylabel('Frequency')
-    ax.set_title(f'{label} Model - Residuals Histogram')
-    if fold is not None:
-        plt.savefig(f'/home/s1949330/Documents/scratch/diss_data/model/{folder}/{label}_MODEL_HIST_FOLD{fold}.png')
-    else:
-        plt.savefig(f'/home/s1949330/Documents/scratch/diss_data/model/{folder}/{label}_MODEL_HIST.png')
+    plt.hist2d(y_test, y_pred, bins = (100,100), cmap = 'turbo', cmin = 5)
+    plt.colorbar(shrink = 0.75)
+    ax.plot([0,300], [0,300], ls = 'solid', color = 'k')
+    # Plot line of best fit
+    slope, intercept = np.polyfit(y_test, y_pred, 1)
+    best_fit = slope * np.array(y_test) + intercept
+    ax.plot(y_test, best_fit, ls = 'solid', color = 'red', label = 'Linear')
+    ax.set_xlim([0, 300])
+    ax.set_ylim([0, 300])
+    ax.set_xticks(np.arange(0, 310, 10))
+    ax.set_yticks(np.arange(0, 310, 10))
+    ax.set_xlabel('Observed AGB (Mg/ha)')
+    ax.set_ylabel('Predicted AGB (Mg/ha)')
+    ax.set_title(f'{label} Model - Observed vs Predicted')
+    plt.savefig(f'/home/s1949330/Documents/scratch/diss_data/model/{folder}/{label}_MODEL_HIST_FOLD{model + 1}.png', dpi = 300)
     plt.close(fig)
 
-def cross_validation(x, y, k, label, trees, folder):
-    '''Perform K-Fold Cross-Validation and Save Statistics'''
+def cross_validation(x, y, sample, kfolds, label, trees, folder):
+    '''Train Model with K-Fold Cross-Validation'''
     # Configure k-fold cross validation
-    kf = KFold(n_splits = k, shuffle = True, random_state = random.seed())
+    kf = KFold(n_splits = kfolds, shuffle = True, random_state = random.seed())
     stats_list = []
+    if sample:
+        sample_indices = random.sample(range(len(x)), k = int(0.05 * len(x)))
+        x = x.iloc[sample_indices]
+        y = y.iloc[sample_indices]
+        print('Training Data Sample Size: {:,}'.format(len(x)))
     # Perform cross validation
     for fold, (train_index, test_index) in enumerate(kf.split(x)):
-        print(f'Fold {fold + 1}/{k}')
+        print(f'Fold {fold + 1}/{kfolds}')
         x_train, x_test = x.iloc[train_index], x.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
         # Train model
-        train_model(x_train, y_train, label, trees, folder, fold + 1)
+        train_model(x_train, y_train, label, trees, folder, fold)
         # Test model
-        y_pred, stats_dict = test_model(x_test, y_test, folder, label, fold + 1)
+        y_pred, stats_dict = test_model(x_test, y_test, folder, label, fold)
         stats_dict['fold'] = fold + 1
         stats_list.append(stats_dict)
         # Save splits
-        save_splits(x_train, y_train, x_test, y_test, x.index.to_series(), args, fold + 1)
+        save_splits(x_train, y_train, x_test, y_test, x.index.to_series(), args, fold)
+        # Visualise model performance
+        model_scatter(y_test, y_pred, folder, label, model = fold)
+        model_hist(y_test, y_pred, folder, label, model = fold)
+        # Store variable importances
+        variable_importance(folder, label, x.columns, fold)
+        fold += 1
     # Save statistics to csv
     stats_df = pd.DataFrame(stats_list)
     stats_df.to_csv(f'/home/s1949330/Documents/scratch/diss_data/model/{folder}/{label}_KFOLD_STATS.csv', index = False)
@@ -203,44 +218,21 @@ def cross_validation(x, y, k, label, trees, folder):
 if __name__ == '__main__':
     
     # Define command line arguments
-    parser = argparse.ArgumentParser(description = 'Random Forest Model Training and Evaluation')
+    parser = argparse.ArgumentParser(description = 'Random Forest Model Training with K-Fold Cross Validation')
+    parser.add_argument('--site', default = '', help = 'Site-specific identifier for model training')
     parser.add_argument('--label', required = True, help = 'Predictor label (e.g., Landsat, Sentinel, Palsar, All, Test)')
-    parser.add_argument('--site', default = '', help = 'Site-specific identifier')
     parser.add_argument('--folder', required = True, help = 'Folder to save results')
+    parser.add_argument('--kfolds', type = int, required = True, help = 'Number of k-folds for cross-validation')
     parser.add_argument('--sample', action = 'store_true', help = 'Adopt a smaller sample size of the available training data')
-    parser.add_argument('--trees', type = int, default = 200, help = 'Number of trees in the random forest, default 200')
-    parser.add_argument('--split', type = float, default = 0.3, help = 'Test split ratio, default 0.3')
-    parser.add_argument('--kfolds', type = int, default = 5, help = 'Number of k-folds for cross-validation, default, 5')
-    parser.add_argument('--fold_model', type = int, default = None, help = 'Fold number to evaluate specific model')
+    parser.add_argument('--trees', type = int, default = 200, help = 'Number of trees in the random forest')
+    parser.add_argument('--split', type = float, default = 0.3, help = 'Test split ratio')
     args = parser.parse_args()
 
     # Isolate target and predictor variables
     input_filename = f'/home/s1949330/Documents/scratch/diss_data/pred_vars/input_final/{args.site}_MODEL_INPUT_FINAL.csv' if args.site else '/home/s1949330/Documents/scratch/diss_data/pred_vars/input_final/MODEL_INPUT_FINAL.csv'
     y, x, coords = isolate_data(input_filename, args.label)
 
-    if args.kfolds:
-        # Perform k-fold cross validation 
-        cross_validation(x, y, args.kfolds, args.label, args.trees, args.folder)
-    else:
-        if args.fold_model is not None:
-            # Evaluate a specific fold model
-            fold = args.fold_model
-            model_filename = f'/home/s1949330/Documents/scratch/diss_data/model/{args.folder}/{args.label}_RF_MODEL_FOLD{fold}.joblib'
-            y_test = pd.read_csv(f'/home/s1949330/Documents/scratch/diss_data/model/{args.folder}/{args.label}_MODEL_TESTING_FOLD{fold}.csv')['y_test']
-            x_test = pd.read_csv(f'/home/s1949330/Documents/scratch/diss_data/model/{args.folder}/{args.label}_MODEL_TESTING_FOLD{fold}.csv').drop(columns = ['y_test'])
-            y_pred, _ = test_model(x_test, y_test, args.folder, args.label, fold)
-            model_scatter(y_test, y_pred, args.folder, args.label, fold)
-            model_hist(y_test, y_pred, args.folder, args.label, fold)
-            variable_importance(args.folder, args.label, x.columns, fold)
-        else:
-            # Split data for model training
-            x_train, x_test, y_train, y_test = split_data(x, y, args.split, args.sample)
-            save_splits(x_train, y_train, x_test, y_test, coords, args)
-            # Train RF model
-            train_model(x_train, y_train, args.label, args.trees, args.folder)
-            # Test RF model performance
-            y_pred, stats_dict = test_model(x_test, y_test, args.folder, args.label)
-            model_scatter(y_test, y_pred, args.folder, args.label)
-            model_hist(y_test, y_pred, args.folder, args.label)
-            # Save variable importances
-            variable_importance(args.folder, args.label, x.columns)
+    # Perform k-fold cross validation for model training    
+    cross_validation(x, y, args.sample, args.kfolds, args.label, args.trees, args.folder)
+
+
