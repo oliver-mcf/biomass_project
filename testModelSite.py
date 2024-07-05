a454ac2404fc2e931938e4685e5bd29123d6494a
s1949330 <s1949330@ed.ac.uk>
2024-07-05 16:53:48 +0100

    default trees set to 100 as predictor variables now 35, input csv filenames also adjusted, and model stats stored in csv

--- testModelSite.py
+++ testModelSite.py
@@ -18,16 +18,16 @@ if __name__ == '__main__':
     parser.add_argument('--folder', required = True, help = 'Folder to save results')
     parser.add_argument('--trainSite', required = True, help = 'Site for training data')
     parser.add_argument('--testSite', required = True, help = 'Site for testing data')
-    parser.add_argument('--trees', type = int, default = 200, help = 'Number of trees in the random forest')
+    parser.add_argument('--trees', type = int, default = 100, help = 'Number of trees in the random forest')
     parser.add_argument('--split', type = float, default = 0.3, help = 'Split ratio for input site data')
     args = parser.parse_args()
 
     # Isolate target and predictor variables for training data
-    train_filename = f'/home/s1949330/Documents/scratch/diss_data/pred_vars/input_final/{args.trainSite}_MODEL_INPUT_FINAL.csv'
+    train_filename = f'/home/s1949330/Documents/scratch/diss_data/pred_vars/input_final/{args.label}_{args.trainSite}_MODEL_INPUT_FINAL.csv'
     y_train, x_train, coords_train = isolate_data(train_filename, args.label)
 
     # Isolate target and predictor variables for testing data
-    test_filename = f'/home/s1949330/Documents/scratch/diss_data/pred_vars/input_final/{args.testSite}_MODEL_INPUT_FINAL.csv'
+    test_filename = f'/home/s1949330/Documents/scratch/diss_data/pred_vars/input_final/{args.label}_{args.testSite}_MODEL_INPUT_FINAL.csv'
     y_test, x_test, coords_test = isolate_data(test_filename, args.label)
 
     # Train the model
@@ -37,7 +37,7 @@ if __name__ == '__main__':
     y_pred, stats_dict = test_model(x_test, y_test, args.folder, args.label)
     # Save statistics to csv
     stats_df = pd.DataFrame(stats_dict)
-    stats_df.to_csv(f'/home/s1949330/Documents/scratch/diss_data/model/{args.folder}/{args.label}_KFOLD_STATS.csv', index = False)
+    stats_df.to_csv(f'/home/s1949330/Documents/scratch/diss_data/model/{args.folder}/{args.label}_{args.trainSite}-{args.testSite}_STATS.csv', index = False)
 
     # Visualize model performance
     model_scatter(y_test, y_pred, args.folder, args.label, model = 0, single_output = True)
@@ -45,3 +45,4 @@ if __name__ == '__main__':
 
     # Store variable importances
     variable_importance(args.folder, args.label, x_train.columns)
+ 
