9d283f70c50d7658ff0a87e9efa424f616068e73
s1949330 <s1949330@ed.ac.uk>
2024-07-05 17:28:53 +0100

    spatial cross validation function amended from main cross_validation, now k-fold cv for site transferability test

--- testModelSite.py
+++ testModelSite.py
@@ -6,7 +6,43 @@ from libraries import *
 
 
 # Objects & Methods ################################################################################################
-from trainModel import isolate_data, train_model, test_model, model_scatter, model_hist, variable_importance
+from trainModel import *
+
+def spatial_cv(train_filename, test_filename, kfolds, label, trees, folder, sample):
+    '''Train Model with K-Fold Cross-Validation'''
+    # Load data from separate sites
+    y_train, x_train, _ = isolate_data(train_filename, label)
+    y_test, x_test, _ = isolate_data(test_filename, label)
+    # Configure k-fold cross validation
+    kf = KFold(n_splits = kfolds, shuffle = True, random_state = random.seed())
+    stats_list = []
+    if sample:
+        sample_indices = random.sample(range(len(x_train)), k = int(0.05 * len(x_train)))
+        x_train = x_train.iloc[sample_indices]
+        y_train = y_train.iloc[sample_indices]
+        print('Training Data Sample Size: {:,}'.format(len(x_train)))
+    # Perform cross validation
+    for fold, (train_index, val_index) in enumerate(kf.split(x_train)):
+        print(f'Fold {fold + 1}/{kfolds}')
+        x_train_fold, x_val_fold = x_train.iloc[train_index], x_train.iloc[val_index]
+        y_train_fold, y_val_fold = y_train.iloc[train_index], y_train.iloc[val_index]
+        # Train model
+        train_model(x_train_fold, y_train_fold, label, trees, folder, fold)
+        # Test model
+        y_pred, stats_dict = test_model(x_test, y_test, folder, label, fold)
+        stats_dict['Fold'] = fold + 1
+        stats_list.append(stats_dict)
+        # Save splits
+        save_splits(x_train_fold, y_train_fold, x_val_fold, y_val_fold, x_train.index.to_series(), args, fold)
+        # Visualise model performance
+        model_scatter(y_test, y_pred, folder, label, model = fold)
+        model_hist(y_test, y_pred, folder, label, model = fold)
+        # Store variable importances
+        variable_importance(folder, label, x_train.columns, fold)
+    # Save statistics to csv
+    stats_df = pd.DataFrame(stats_list)
+    stats_df.to_csv(f'/home/s1949330/Documents/scratch/diss_data/model/{folder}/{label}_KFOLD_STATS.csv', index = False)
+
 
 
 # Code #############################################################################################################
@@ -18,31 +54,16 @@ if __name__ == '__main__':
     parser.add_argument('--folder', required = True, help = 'Folder to save results')
     parser.add_argument('--trainSite', required = True, help = 'Site for training data')
     parser.add_argument('--testSite', required = True, help = 'Site for testing data')
+    parser.add_argument('--kfolds', type = int, required = True, help = 'Number of k-folds for cross-validation')
+    parser.add_argument('--sample', action = 'store_true', help = 'Adopt a smaller sample size of the available training data')
     parser.add_argument('--trees', type = int, default = 100, help = 'Number of trees in the random forest')
     parser.add_argument('--split', type = float, default = 0.3, help = 'Split ratio for input site data')
     args = parser.parse_args()
 
-    # Isolate target and predictor variables for training data
-    train_filename = f'/home/s1949330/Documents/scratch/diss_data/pred_vars/input_final/{args.label}_{args.trainSite}_MODEL_INPUT_FINAL.csv'
-    y_train, x_train, coords_train = isolate_data(train_filename, args.label)
+    # Define train and test site filenames
+    train_site = f'/home/s1949330/Documents/scratch/diss_data/pred_vars/input_final/{args.label}_{args.trainSite}_MODEL_INPUT_FINAL.csv'
+    test_site = f'/home/s1949330/Documents/scratch/diss_data/pred_vars/input_final/{args.label}_{args.testSite}_MODEL_INPUT_FINAL.csv'
 
-    # Isolate target and predictor variables for testing data
-    test_filename = f'/home/s1949330/Documents/scratch/diss_data/pred_vars/input_final/{args.label}_{args.testSite}_MODEL_INPUT_FINAL.csv'
-    y_test, x_test, coords_test = isolate_data(test_filename, args.label)
-
-    # Train the model
-    train_model(x_train, y_train, args.label, args.trees, args.folder)
-
-    # Test the model
-    y_pred, stats_dict = test_model(x_test, y_test, args.folder, args.label)
-    # Save statistics to csv
-    stats_df = pd.DataFrame(stats_dict)
-    stats_df.to_csv(f'/home/s1949330/Documents/scratch/diss_data/model/{args.folder}/{args.label}_{args.trainSite}-{args.testSite}_STATS.csv', index = False)
-
-    # Visualize model performance
-    model_scatter(y_test, y_pred, args.folder, args.label, model = 0, single_output = True)
-    model_hist(y_test, y_pred, args.folder, args.label, model = 0, single_output = True)
-
-    # Store variable importances
-    variable_importance(args.folder, args.label, x_train.columns)
+    # Perform spatial k-fold cross validation
+    spatial_cv(train_site, test_site, args.kfolds, args.label, args.trees, args.folder, args.sample)
  
