98bfbcd63ac2053a4af70bd467e573bd3d39a847
s1949330 <s1949330@ed.ac.uk>
2024-07-15 17:27:09 +0100

    added linear regression module

--- libraries.py
+++ libraries.py
@@ -15,6 +15,7 @@ from itertools import zip_longest
 from sklearn.model_selection import train_test_split, KFold
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, explained_variance_score
+from sklearn.linear_model import LinearRegression
 import joblib
 import random
 from math import sqrt
