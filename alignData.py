0b1f113900c2c02221560c2944c7eedbb1aa1a92
s1949330 <s1949330@ed.ac.uk>
2024-07-16 15:28:25 +0100

    working geolocation filter options

--- alignData.py
+++ alignData.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
     # Define command line arguments
     parser = argparse.ArgumentParser(description = 'Combine CSV files for model training')
     parser.add_argument('--site', help = 'Site name to filter CSV files')
-    parser.add_argument('--geo', help = 'End of folder name for either "PALSAR" or "COVER" geolocation filter')
+    parser.add_argument('--geo', help = 'Either "PALSAR" or "COVER" geolocation filter')
     args = parser.parse_args()
 
     # Identify filtered input data
