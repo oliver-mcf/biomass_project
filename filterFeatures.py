fe2866de42689fbb867fb712a2f8cdb131f8b193
s1949330 <s1949330@ed.ac.uk>
2024-07-16 16:38:17 +0100

    working script to accomodate different geolocation filtered csv

--- filterFeatures.py
+++ filterFeatures.py
@@ -8,9 +8,10 @@ from libraries import *
 # Objects & Methods ################################################################################################
 from trainModel import isolate_data
 
-def matrix(df, label, coef, output_dir, output_csv):
+def matrix(df, label, geo, coef, output_dir, output_csv):
     '''Create correlation matrix for predictor variables and filter highly correlated ones.'''
     # Compute the correlation matrix allowing for various procesisng stages
+    df = df.drop(columns = ['GEDI_COVER'])
     gedi_columns = ['Source', 'GEDI_X', 'GEDI_Y', 'GEDI_AGB']
     predictor_columns = [col for col in df.columns if col not in gedi_columns]
     correlation_matrix = df[predictor_columns].corr()
@@ -24,7 +25,11 @@ def matrix(df, label, coef, output_dir, output_csv):
                 cbar_kws = {"shrink": .5}, mask = np.eye(len(correlation_matrix), dtype = bool),
                 vmin = -1, vmax = 1)
     plt.title('Predictor Variable Correlation Matrix')
-    plt.savefig(f'{output_dir}{label}_CORRELATION_MATRIX.png', dpi = 300)
+    if geo:
+        output_fig = f'{output_dir}{label}_CORRELATION_MATRIX_{geo}.png'
+    else:   
+        output_fig = f'{output_dir}{label}_CORRELATION_MATRIX.png'
+    plt.savefig(output_fig, dpi = 300)
     plt.close()
     # Identify and remove highly correlated variables
     variables_to_keep = set(predictor_columns)
@@ -39,7 +44,11 @@ def matrix(df, label, coef, output_dir, output_csv):
     print("Variables retained:", len(variables_to_keep))
     pprint(variables_to_keep)
     # Save lists of removed and retained variables to a CSV file
-    with open(f'{output_dir}{label}_FILTERED_VARIABLES.csv', mode = 'w', newline = '') as file:
+    if geo:
+        variable_csv = f'{output_dir}{label}_FILTERED_VARIABLES_{geo}.csv'
+    else:
+        variable_csv = f'{output_dir}{label}_FILTERED_VARIABLES.csv'
+    with open(variable_csv, mode = 'w', newline = '') as file:
         writer = csv.writer(file)
         writer.writerow(['Retained', 'Removed'])
         for keep, remove in zip_longest(variables_to_keep, variables_to_remove, fillvalue = ''):
@@ -50,7 +59,7 @@ def matrix(df, label, coef, output_dir, output_csv):
     df_filtered.to_csv(output_csv, index = False)
     print(f"Filtered data saved to: {output_csv}")
 
-def reduce(ref_csv, csv_list, output_dir):
+def reduce(ref_csv, csv_list, output_dir, geo):
     '''Reduce predictor variables by established threshold for sites'''
     # Read already filtered csv
     ref_df = pd.read_csv(ref_csv)
@@ -62,7 +71,10 @@ def reduce(ref_csv, csv_list, output_dir):
         filtered_df = df[common_columns]
         filtered_df = filtered_df[ref_columns]
         # Write newly filtered site input data to csv
-        filename = os.path.basename(csv_file).replace('_GEO.csv', '_GEO_FINAL.csv')
+        if geo:
+            filename = os.path.basename(csv_file).replace(f'MERGE_{geo}.csv', f'FINAL_{geo}.csv')
+        else:
+            filename = os.path.basename(csv_file).replace(f'MERGE.csv', f'FINAL.csv')
         output_path = os.path.join(output_dir, filename)
         filtered_df.to_csv(output_path, index = False)
         print(f"Site filtered data saved to: {output_path}")
@@ -75,6 +87,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description = 'Filtering Predictor Variables for Model Input')
     parser.add_argument('--label', help = 'Predictor label (e.g., Landsat, Sentinel, Palsar, All)')
     parser.add_argument('--filter', action = 'store_true', help = 'Filter predictor variables of main model input csv')
+    parser.add_argument('--geo', help = 'Geolocation filter, PALSAR or COVER')
     parser.add_argument('--coef', type = float, help = 'Correlation coefficient threshold for filtering predictor variables')
     parser.add_argument('--reduce', action = 'store_true', help = 'Reduce predictor variables in site specific data based on previous filtering')
     args = parser.parse_args()
@@ -83,18 +96,28 @@ if __name__ == '__main__':
     if args.filter:
         
         # Isolate variables by group/label given
-        csv_file = f'/home/s1949330/Documents/scratch/diss_data/pred_vars/input_merge/All_MODEL_INPUT_GEO.csv'
+        if args.geo:
+            csv_file = f'/home/s1949330/data/diss_data/pred_vars/input_merge/All_EXTRACT_MERGE_{args.geo}.csv'
+            output_csv = f'/home/s1949330/data/diss_data/pred_vars/input_final/{args.label}_EXTRACT_FINAL_{args.geo}.csv'
+        else: 
+            csv_file = '/home/s1949330/data/diss_data/pred_vars/input_merge/All_EXTRACT_MERGE.csv'
+            output_csv = f'/home/s1949330/data/diss_data/pred_vars/input_final/{args.label}_EXTRACT_FINAL.csv'
         df = isolate_data(csv_file, args.label, filter = True)
 
         # Perform correlation matrix and remove pairs above threshold
-        output_csv = f'/home/s1949330/Documents/scratch/diss_data/pred_vars/input_final/{args.label}_MODEL_INPUT_GEO_FINAL.csv'
-        output_dir = f'/home/s1949330/Documents/scratch/diss_data/pred_vars/input_final/'
-        matrix(df, args.label, args.coef, output_dir, output_csv)
+        output_dir = f'/home/s1949330/data/diss_data/pred_vars/input_final/'
+        matrix(df, args.label, args.geo, args.coef, output_dir, output_csv)
         
     # Reduce site data to match filtered predictor variables
     if args.reduce:
-        csv_list = ['/home/s1949330/Documents/scratch/diss_data/pred_vars/input_merge/All_MGR_MODEL_INPUT_GEO.csv', 
-                    '/home/s1949330/Documents/scratch/diss_data/pred_vars/input_merge/All_TKW_MODEL_INPUT_GEO.csv']
-        ref_csv = '/home/s1949330/Documents/scratch/diss_data/pred_vars/input_final/All_MODEL_INPUT_GEO_FINAL.csv'
-        output_dir = '/home/s1949330/Documents/scratch/diss_data/pred_vars/input_final/'
-        reduce(ref_csv, csv_list, output_dir)
+        
+        if args.geo:
+            csv_list = [f'/home/s1949330/data/diss_data/pred_vars/input_merge/All_MGR_EXTRACT_MERGE_{args.geo}.csv', 
+                        f'/home/s1949330/data/diss_data/pred_vars/input_merge/All_TKW_EXTRACT_MERGE_{args.geo}.csv']
+            ref_csv = f'/home/s1949330/data/diss_data/pred_vars/input_final/All_EXTRACT_FINAL_{args.geo}.csv'
+        else:
+            csv_list = [f'/home/s1949330/data/diss_data/pred_vars/input_merge/All_MGR_EXTRACT_MERGE.csv', 
+                        f'/home/s1949330/data/diss_data/pred_vars/input_merge/All_TKW_EXTRACT_MERGE.csv']
+            ref_csv = '/home/s1949330/data/diss_data/pred_vars/input_final/All_EXTRACT_FINAL.csv'
+        output_dir = '/home/s1949330/data/diss_data/pred_vars/input_final/'
+        reduce(ref_csv, csv_list, output_dir, args.geo)
