6df452d699fb996821247ac3eba692703e166fa6
s1949330 <s1949330@ed.ac.uk>
2024-07-19 17:09:05 +0100

    regression csvs moved to input_merge folder

--- assessData.py
+++ assessData.py
@@ -47,9 +47,9 @@ def var_regress(file, geo):
     # Convert results dictionary to a DataFrame
     results_df = pd.DataFrame(results)
     if geo:
-        output_csv = f'/home/s1949330/data/diss_data/pred_vars/input_final/All_LINEAR_REGRESSION_{geo}.csv'
+        output_csv = f'/home/s1949330/data/diss_data/pred_vars/input_merge/All_LINEAR_REGRESSION_{geo}.csv'
     else:
-        output_csv = '/home/s1949330/data/diss_data/pred_vars/input_final/All_LINEAR_REGRESSION.csv'
+        output_csv = '/home/s1949330/data/diss_data/pred_vars/input_merge/All_LINEAR_REGRESSION.csv'
     results_df.to_csv(output_csv, index = False)
     print(f'SUCCESS: Regression results saved to {output_csv}')
 
