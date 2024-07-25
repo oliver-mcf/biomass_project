36140274787370b78697dfcab11a8efc03f3edf1
s1949330 <s1949330@ed.ac.uk>
2024-07-25 17:21:54 +0100

    working script for all intersecting pixels by site and year

--- validateBiomass.py
+++ validateBiomass.py
@@ -7,7 +7,7 @@ from extractData import GeoTiff
 
 # Objects & Methods ################################################################################################
 
-def read_plots(csv_file, year = None, site = None):
+def read_plots(csv_file, site = None, year = None):
     '''Read Field AGB Estimates'''
     df = pd.read_csv(csv_file)
     if site:
@@ -37,34 +37,103 @@ def intersect_plots(gedi, field_df):
         if 0 <= xInd < gedi.nX and 0 <= yInd < gedi.nY:
             pixel_value = gedi.data[yInd, xInd]
             plot_intersect[i] = pixel_value
-    return plot_intersect
+    field_df['GEDI_AGB'] = plot_intersect
+    return field_df
 
+def validation_stats(field, gedi, site, year):
+    '''Statistically Validate GEDI AGB with field AGB'''
+    residuals = gedi - field
+    constant = sm.add_constant(field)
+    white_test = sm.stats.diagnostic.het_white(residuals, constant)
+    stats_dict = {
+        'R2 (r2_score)': r2_score(field, gedi),
+        'R2 (manual)': stats.pearsonr(field, gedi)[0] ** 2,
+        'R2 (variance)': explained_variance_score(field, gedi),
+        'Bias': np.sum(gedi - field) / len(field),
+        'MAE': np.mean(np.abs(field - gedi)),
+        'MAE%': np.mean(np.abs(field - gedi)) / np.mean(field) * 100,
+        'RMSE': sqrt(mean_squared_error(field, gedi)),
+        'RMSE%': sqrt(mean_squared_error(field, gedi)) / np.mean(field) * 100,
+        'R': stats.pearsonr(field, gedi)[0],
+        'LM': white_test[0],
+        'F': white_test[2],
+        'P': white_test[1]}
+    valid_stats = []
+    valid_stats.append(stats_dict)
+    stats_df = pd.DataFrame(valid_stats)
+    pprint(stats_df)
+    stats_df.to_csv(f'/home/s1949330/data/diss_data/model/yes_geo/All/predict/validate/{site}_{year}_VALIDATION_STATS.csv', index = False)
 
-# compute statistical metrics
-
-# plot scatter graph
-
+def validation_plot(field, gedi, site, year):
+    '''Plot Scatter of Observed and Predicted Values'''
+    # Plot scatter
+    plt.rcParams['font.family'] = 'Arial'
+    fig, ax = plt.subplots(figsize = (8,8))
+    ax.scatter(field, gedi, marker = '.', color = 'tab:blue')
+    ax.plot([0, 150], [0, 150], ls = 'solid', color = 'k')
+    # Plot line of best fit
+    slope, intercept = np.polyfit(field, gedi, 1)
+    best_fit = slope * np.array(field) + intercept
+    ax.plot(field, best_fit, ls = 'solid', color = 'red', label = 'Linear')
+    ax.set_xlim([0, 150])
+    ax.set_ylim([0, 150])
+    ax.set_xticks(np.arange(0, 150 + 10, 10))
+    ax.set_yticks(np.arange(0, 150 + 10, 10))
+    ax.set_xlabel('Field AGB Estimates (Mg/ha)')
+    ax.set_ylabel('Extrapolated GEDI AGB Estimates (Mg/ha)')
+    ax.set_title(f'RFA Model Validation')
+    fig_name = f'/home/s1949330/data/diss_data/model/yes_geo/All/predict/validate/{site}_{year}_VALIDATION_PLOT.png'
+    plt.savefig(fig_name, dpi = 300)
+    plt.close(fig)
 
 # Code #############################################################################################################
 if __name__ == '__main__':
 
-    # Define command line arguments
-    parser = argparse.ArgumentParser(description = 'Perform In-Situ Validation of GEDI AGB Estimates.')
-    parser.add_argument('--year', help = 'Isolate SEOSAW data to given austral year: 17, 18, 21.')
-    parser.add_argument('--site', type = str, help = 'SEOSAW study site: MGR or TKW.')
-    args = parser.parse_args()
+    # Initialise validation
+    validation = pd.DataFrame(columns = ["Site", "Year", "X", "Y", "GEDI_AGB", "Field_AGB"])
+    year_list = ['17', '21']
+    site_list = ['MGR', 'TKW']
+    
+    for site in site_list:
+        for year in year_list:
+            
+            # Read field AGB estimates
+            csv_file = '/home/s1949330/data/diss_data/seosaw/seosaw_agb.csv'
+            field_df = read_plots(csv_file, site, year)
 
-    # Read field AGB estimates
-    csv_file = '/home/s1949330/data/diss_data/seosaw/seosaw_agb.csv'
-    field_df = read_plots(csv_file, args.year, args.site)
-    pprint(field_df)
+            # Read GEDI AGB estimates
+            pred = f'/home/s1949330/data/diss_data/model/yes_geo/All/predict/{site}_{year}_PREDICT_AGB_COVER.tif'
+            gedi_obj = GeoTiff(pred)
 
-    # Read GEDI AGB estimates
-    pred = f'/home/s1949330/data/diss_data/model/yes_geo/All/predict/{args.site}_{args.year}_PREDICT_AGB_COVER.tif'
-    gedi = GeoTiff(pred)
+            # Isolate GEDI AGB estimates in plots
+            intersect_df = intersect_plots(gedi_obj, field_df)
 
-    # Isolate GEDI AGB estimates in plots
-    plot_intersect = intersect_plots(gedi, field_df)
-    print(plot_intersect)
+            # Calculate validation statistics
+            gedi = intersect_df['GEDI_AGB']
+            field = intersect_df[f'AGB_{year}']
+            validation_stats(field, gedi, site, year)
+            validation_plot(field, gedi, site, year)
 
+            # Append intersecting data to the validation DataFrame
+            for i in range(len(intersect_df)):
+                validation = validation.append({
+                    "Site": site,
+                    "Year": year,
+                    "X": intersect_df.iloc[i]['X'],
+                    "Y": intersect_df.iloc[i]['Y'],
+                    "GEDI_AGB": intersect_df.iloc[i]['GEDI_AGB'],
+                    "Field_AGB": field.iloc[i]
+                }, ignore_index = True)
+
+    # Save combined data to CSV
+    pprint(validation)
+    validation.to_csv('/home/s1949330/data/diss_data/model/yes_geo/All/predict/validate/BOTH_ALL_VALIDATION.csv', index = False)
+
+    # Calculate statistics for all validation data
+    all_gedi = validation['GEDI_AGB']
+    all_field = validation['Field_AGB']
+    validation_stats(all_field, all_gedi, 'BOTH', 'ALL')
+
+    # Visualise all validation data on a scatter plot
+    validation_plot(all_field, all_gedi, 'BOTH', 'ALL')
     
